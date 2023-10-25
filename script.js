066777de47d9bf22910e41ef59c121add330ff90
Hacker94 <83153199+Ketan-vscode@users.noreply.github.com>
2023-10-25 12:24:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+LeftWristX = 0;
+LeftWristY = 0;
+RightWristX = 0;
+RightWristY = 0;
 
 function preload() {
     song1 = loadSound("music.mp3");
@@ -12,6 +16,26 @@ function setup() {
 
     capture = createCapture(VIDEO);
     capture.hide();
+
+    poseNet = ml5.poseNet(capture , modeLoaded);
+    poseNet.on("pose" , gotPoses);
+}
+
+function modeLoaded(){
+    console.log("Pose Net is intitalized");
+}
+
+function gotPoses(){
+    if(results.length > 0){
+        console.log(results);
+        LeftWristX = results[0].pose.leftWrist.x;
+        LeftWristY = results[0].pose.leftWrist.y;
+        console.log(" Left Wrist X = " + LeftWristX + " Left Wrist Y = " + LeftWristY);
+
+        RightWristX = results[0].pose.rightWrist.x;
+        RightWristY = results[0].pose.rightWrist.y;
+        console.log(" Right Wrist X = " + RightWristX + "Right Wrist Y = " + RightWristY);
+    }
 }
 
 function draw() {
@@ -21,4 +45,3 @@ function draw() {
 function play(){
     music.play();
 }
-
